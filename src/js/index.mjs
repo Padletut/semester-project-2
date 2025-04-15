8c9f86e217f1bd28b1ab0a815af1c885e76dc5cf
Robert Nilsen <93103203+Padletut@users.noreply.github.com>
2025-04-15 23:23:35 +0200

    Add authorization page with login and signup functionality, update navigation links, and include necessary styles

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,2 +1,23 @@
 import "../scss/index.scss";
 import * as bootstrap from "bootstrap";
+
+// Switch between Login and Register account tabs
+
+const signUpLink = document.getElementById("signUpLink");
+const signInLink = document.getElementById("signInLink");
+
+if (signUpLink) {
+  signUpLink.addEventListener("click", function (event) {
+    event.preventDefault();
+    const signUpTab = new bootstrap.Tab(document.getElementById("signUp-tab"));
+    signUpTab.show();
+  });
+}
+
+if (signInLink) {
+  signInLink.addEventListener("click", function (event) {
+    event.preventDefault();
+    const signInTab = new bootstrap.Tab(document.getElementById("signIn-tab"));
+    signInTab.show();
+  });
+}
