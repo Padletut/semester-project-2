24d498d33e575cbe74f0f389a18face31aa8ed19
Robert Nilsen <93103203+Padletut@users.noreply.github.com>
2025-04-17 01:59:44 +0200

    Refactor SCSS imports for consistency and update Vite configuration for improved path resolution

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,11 +2,18 @@ import { resolve } from "path";
 
 export default {
   root: resolve(__dirname, "src"),
-  build: {
-    outDir: "../dist",
-    emptyOutDir: true,
+  resolve: {
+    alias: {
+      "~bootstrap": resolve(__dirname, "node_modules/bootstrap"),
+      "~bootstrap-icons": resolve(__dirname, "node_modules/bootstrap-icons"),
+    },
   },
   server: {
     port: 5000,
+    open: true,
+  },
+  build: {
+    outDir: resolve(__dirname, "dist"),
+    emptyOutDir: true,
   },
 };
