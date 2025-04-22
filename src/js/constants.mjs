5cb924a85b3c1052b6a32334b1521c4501e1ba02
Robert Nilsen <93103203+Padletut@users.noreply.github.com>
2025-04-22 02:53:43 +0200

    fix: update API_KEY assignment to use import.meta.env for compatibility

--- src/js/constants.mjs
+++ src/js/constants.mjs
@@ -1,4 +1,4 @@
-export const API_KEY = process.env.API_KEY;
+export const API_KEY = import.meta.env.VITE_API_KEY;
 export const API_BASE_URL = "https://v2.api.noroff.dev";
 export const API_AUTH = "/auth";
 export const API_REGISTER = "/register";
