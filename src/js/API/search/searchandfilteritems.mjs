7b1bc58cac4203b8d27d740682ef6b958362d1e9
Robert Nilsen <93103203+Padletut@users.noreply.github.com>
2025-05-25 21:55:28 +0200

    refactor: moved rerenderItems function out of searchAndFilterItems class

--- src/js/API/search/searchandfilteritems.mjs
+++ src/js/API/search/searchandfilteritems.mjs
@@ -6,6 +6,7 @@ import * as constants from "../../constants.mjs";
 import { ERROR_MESSAGES } from "../utils/errormessages.mjs";
 import { toggleLoader } from "../../ui/shared/toggleLoader.mjs";
 import { fetchAndRenderFilteredItems } from "./fetchAndRenderFilteredItems.mjs";
+import { rerenderItems } from "./rerenderItems.mjs";
 
 /**
  * Class representing a search and filter functionality for items.
@@ -49,6 +50,7 @@ export class SearchAndFilterItems {
     this.sentinel = document.getElementById("sentinel");
 
     this.fetchAndRenderFilteredItems = () => fetchAndRenderFilteredItems(this);
+    this.rerenderItems = () => rerenderItems(this);
     this.setupEventListeners();
   }
   /**
@@ -140,51 +142,7 @@ export class SearchAndFilterItems {
 
     observer.observe(this.sentinel); // Start observing the sentinel
   }
-  /**
-   * Rerenders the items in the container based on the current search and filter state.
-   * This method fetches items from the API and updates the DOM accordingly.
-   * @private
-   * @example
-   * ```javascript
-   * this.rerenderItems();
-   * ```
-   */
-  async rerenderItems() {
-    if (this.searchInput.value.trim() || this.filterInput.value.trim()) {
-      return;
-    }
 
-    try {
-      toggleLoader(true, this.loaderContainer); // Show loader
-      const queryParams = this.createQueryParams({ limit: "10" }); // Set limit to 10 for default items
-      const items = await this.fetchPage(queryParams);
-
-      // Clear the container before rendering
-      this.itemsContainer.innerHTML = "";
-
-      // Render the items
-      items.forEach((item) => {
-        const card = renderListingCard(item, this.itemsContainer);
-        if (card) {
-          this.fragment.appendChild(card); // Append the card to the fragment
-        } else {
-          console.error("Failed to create card for item:", item);
-        }
-      });
-
-      // Append the fragment to the itemsContainer
-      this.itemsContainer.appendChild(this.fragment);
-      this.initializeObserver();
-      this.attachCardClickListeners();
-
-      window.scrollTo({ top: 0, behavior: "smooth" });
-    } catch (error) {
-      renderErrors(new Error(ERROR_MESSAGES.LOADING_PAGE_ERROR));
-      console.error("Error rendering items:", error);
-    } finally {
-      toggleLoader(false, this.loaderContainer); // Hide loader
-    }
-  }
   /**
    * Handles the search form submission event.
    * This method fetches items based on the search query and updates the DOM accordingly.
